33d4d41c6c341ebb70b91dd0a4101db42add42b9
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-09-04 16:10:24 +0200

    add Aufgabe 3

--- main.dart
+++ main.dart
@@ -1,16 +1,36 @@
 import "hello_world.dart";
 import "guten_tag_name.dart";
+import "person.dart";
 
 void main() {
   // 1. Aufgabe
-  // GutenTag hello = GutenTag();
-  // hello.printMessage();
+  GutenTag hello = GutenTag();
+  hello.printMessage();
 
   // 2. Aufgabe
-  var person = Person("Fadi");
+  var person = PersonenName("Fadi");
   person.Mensch();
   person.setName("Johann");
   person.Mensch();
 
   // 3. Aufgabe
+
+  var person1 =
+      PersonEigenschaften("Fadi Bdiwi", 26, "Spielstraße 1234, Kongo");
+  var person2 = PersonEigenschaften("Anna Nas", 25, "Spielstraße 1236, Kongo");
+
+  print("Informationen von Person 1:");
+  person1.ausgabe();
+  print("");
+
+  print("Informationen von Person 2:");
+  person2.ausgabe();
+  print("");
+
+  person1.setName("Johann Nita");
+  person1.setAlter(3900);
+  person1.setAdresse("Breite Straße 4397, Berlin");
+
+  print("Aktualisierte Informationen von Person 1:");
+  person1.ausgabe();
 }
