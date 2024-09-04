3776502e7101f91f957ab63773c62c25c4e355c1
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-09-04 15:18:36 +0200

    add Aufgabe 2 GutenTag+Name

--- main.dart
+++ main.dart
@@ -1,6 +1,16 @@
 import "hello_world.dart";
+import "guten_tag_name.dart";
 
 void main() {
-  GutenTag hello = GutenTag();
-  hello.printMessage();
+  // 1. Aufgabe
+  // GutenTag hello = GutenTag();
+  // hello.printMessage();
+
+  // 2. Aufgabe
+  var person = Person("Fadi");
+  person.Mensch();
+  person.setName("Johann");
+  person.Mensch();
+
+  // 3. Aufgabe
 }
